19906597075ce64402e10a36a18501ebeaa54864
unknown <tf@tf.tf>
2023-05-10 21:41:01 +0400

    add nullobject realisation registration and authorisation

--- LinuxTestNetApp.Model.Abstractions/Types/RegistrationUser.cs
+++ LinuxTestNetApp.Model.Abstractions/Types/RegistrationUser.cs
@@ -6,7 +6,12 @@ using System.Threading.Tasks;
 
 namespace LinuxTestNetApp.Model.Abstractions.Types
 {
-    internal class RegistrationUser
+    public class RegistrationUser
     {
+        public string FirstName { get; init; }
+        public string LastName { get; init; }
+        public string Login { get; init; }
+        public string Email { get; init; }
+        public string Password { get; init; }
     }
 }
