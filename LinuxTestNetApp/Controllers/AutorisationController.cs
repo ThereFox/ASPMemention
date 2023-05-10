096e7fdb6bde68af27f9555877a3f9b58106bcf5
unknown <tf@tf.tf>
2023-05-10 20:11:24 +0400

    init

---
+++ LinuxTestNetApp/Controllers/AutorisationController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LinuxTestNetApp.Controllers
+{
+    [Controller]
+    [Route("/")]
+    public class AutorisationController : Controller
+    {
+        [HttpGet]
+        [Route("/Registration")]
+        public IActionResult ShowRegistrateForm()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route("/Registration")]
+        public IActionResult Registrate(string login, string password)
+        {
+            
+        }
+
+        [HttpGet]
+        [Route("/Authorisation")]
+        public IActionResult ShowAuthoriseForm()
+        {
+            
+        }
+
+    }
+}
